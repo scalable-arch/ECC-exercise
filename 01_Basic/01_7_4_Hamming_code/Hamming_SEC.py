15494c01d7f40733f44aee305866d41814ef9609
xyz123479 <62287136+xyz123479@users.noreply.github.com>
2023-07-03 12:21:22 +0900

    Update Hamming_SEC.py

--- 01_Basic/01_7_4_Hamming_code/Hamming_SEC.py
+++ 01_Basic/01_7_4_Hamming_code/Hamming_SEC.py
@@ -22,7 +22,7 @@ def main():
 
         # Calculate Syndrome
         codeword_transpose = codeword.transpose()
-        Syndrome=np.matmul(H_Matrix, codeword_transpose)
+        Syndrome=(np.matmul(H_Matrix, codeword_transpose))%2
 
         # Error correction
         for error_index in range(codeword_length):
